c1ab6f90e4ce97ba4e6fd447bbc6f83663045421
Dwi Agustianto <dwi.agustianto@ralali.com>
2019-10-08 11:24:05 +0700

    add: svelte routing

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,18 @@
 <script>
-	export let name;
+	import { Router, Link, Route } from 'svelte-routing'
+	import Home from '@views/home/index.svelte'
+	import Explore from '@views/explore/index.svelte'
+
+	export let url = ''
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
-
-<h1>Hello {name}!</h1>
+<Router url={url}>
+	<nav>
+		<Link to="/">Home</Link>
+    <Link to="explore">Explore</Link>
+	</nav>
+	<div>
+		<Route path="/" component={Home} />
+		<Route path="explore" component="{Explore}" />
+	</div>
+</Router>
---
+++ src/commons/components/NavLink/index.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { Link } from 'svelte-routing'
+
+  export let to = ''
+
+  function getProps({ location, href, isPartiallyCurrent, isCurrent }) {
+    const isActive = href === '/' ? isCurrent : isPartiallyCurrent || isCurrent
+
+    // The object returned here is spread on the anchor element's attributes
+    return isActive ? { class: 'active' } : {}
+  }
+</script>
+
+<Link to={to} getProps={getProps}>
+  <slot />
+</Link>
